5f03d1439017779c611e533f80fb3d14d82b0bb8
Istvan Marki <istv0050@edu.easj.dk>
2017-03-24 20:27:40 +0100

    added EFCore + DbContexts

---
+++ Contexts/MercuriesContext.cs
@@ -0,0 +1,16 @@
+﻿using AirQualityAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirQualityAPI.Contexts
+{
+    public class MercuriesContext : DbContext
+    {
+        public MercuriesContext(DbContextOptions<MercuriesContext> options)
+            : base(options) { }
+
+        public MercuriesContext()
+        {
+        }
+        public DbSet<Mercury> Mercury { get; set; }
+    }
+}
---
+++ Contexts/OzonesContext.cs
@@ -0,0 +1,16 @@
+﻿using AirQualityAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirQualityAPI.Contexts
+{
+    public class OzonesContext : DbContext
+    {
+        public OzonesContext(DbContextOptions<OzonesContext> options)
+            : base(options) { }
+
+        public OzonesContext()
+        {
+        }
+        public DbSet<Ozone> Ozone { get; set; }
+    }
+}
