7982658994661ed1c3ba1b78f2eba6099b6949f3
Istvan Marki <istv0050@edu.easj.dk>
2017-03-24 20:13:56 +0100

    implementing interfaces

---
+++ Repository/MercuriesRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AirQualityAPI.Models;
+
+namespace AirQualityAPI.Repository
+{
+    public class MercuriesRepository : IMercuriesRepository
+    {
+        private static List<Mercury> _mercuriesList = new List<Mercury>();
+
+        public void Add(Mercury mercuryEntry)
+        {
+            _mercuriesList.Add(mercuryEntry);
+        }
+
+        public IEnumerable<Mercury> GetAll()
+        {
+            return _mercuriesList;
+        }
+
+        public Mercury Find(string id)
+        {
+            int matchingId = int.Parse(id);
+            return _mercuriesList.SingleOrDefault(m => m.Mercury_Id == matchingId);
+        }
+
+        public void Remove(string id)
+        {
+            int matchingId = int.Parse(id);
+            var itemToRemove = _mercuriesList.FirstOrDefault(m => m.Mercury_Id == matchingId);
+            if (itemToRemove != null)
+                _mercuriesList.Remove(itemToRemove);
+        }
+
+        public void Update(Mercury mercuryEntry)
+        {
+            var itemToUpdate = _mercuriesList.FirstOrDefault(m => m.Mercury_Id == mercuryEntry.Mercury_Id);
+            if (itemToUpdate != null)
+            {
+                itemToUpdate.Mercury_Id = mercuryEntry.Mercury_Id;
+                itemToUpdate.DateTimeStart = mercuryEntry.DateTimeStart;
+                itemToUpdate.Hg = mercuryEntry.Hg;
+                itemToUpdate.Unit = mercuryEntry.Unit;
+            }
+        }
+    }
+}
---
+++ Repository/OzonesRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AirQualityAPI.Models;
+
+namespace AirQualityAPI.Repository
+{
+    public class OzonesRepository : IOzonesRepository
+    {
+        private static List<Ozone> _ozonesList = new List<Ozone>();
+        public void Add(Ozone ozoneEntry)
+        {
+            _ozonesList.Add(ozoneEntry);
+        }
+
+        public IEnumerable<Ozone> GetAll()
+        {
+            return _ozonesList;
+        }
+
+        public Ozone Find(string id)
+        {
+            int matchingId = int.Parse(id);
+            return _ozonesList.SingleOrDefault(o => o.Ozone_Id == matchingId);
+        }
+
+        public void Remove(string id)
+        {
+            int matchingId = int.Parse(id);
+            var itemToRemove = _ozonesList.FirstOrDefault(o => o.Ozone_Id == matchingId);
+            if (itemToRemove != null)
+                _ozonesList.Remove(itemToRemove);
+        }
+
+        public void Update(Ozone ozoneEntry)
+        {
+            var itemToUpdate = _ozonesList.FirstOrDefault(o => o.Ozone_Id == ozoneEntry.Ozone_Id);
+            if (itemToUpdate != null)
+            {
+                itemToUpdate.Ozone_Id = ozoneEntry.Ozone_Id;
+                itemToUpdate.DateTimeStart = ozoneEntry.DateTimeStart;
+                itemToUpdate.Ozone1 = ozoneEntry.Ozone1;
+                itemToUpdate.Unit = ozoneEntry.Unit;
+            }
+        }
+    }
+}
