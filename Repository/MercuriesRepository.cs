03404c3ae2a94c10a9ee63df87bb4ff823ed1be2
Istvan Marki <istv0050@edu.easj.dk>
2017-03-24 20:44:51 +0100

    switching from static list to DbContext (calling EF)

--- Repository/MercuriesRepository.cs
+++ Repository/MercuriesRepository.cs
@@ -2,41 +2,69 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AirQualityAPI.Contexts;
 using AirQualityAPI.Models;
 
 namespace AirQualityAPI.Repository
 {
     public class MercuriesRepository : IMercuriesRepository
     {
-        private static List<Mercury> _mercuriesList = new List<Mercury>();
+        //private static List<Mercury> _mercuriesList = new List<Mercury>();
+
+
+        private MercuriesContext _context;
+
+        public MercuriesRepository(MercuriesContext context)
+        {
+            _context = context;
+        }
 
         public void Add(Mercury mercuryEntry)
         {
-            _mercuriesList.Add(mercuryEntry);
+            //_mercuriesList.Add(mercuryEntry);
+            _context.Mercury.Add(mercuryEntry);
+            _context.SaveChanges();
         }
 
         public IEnumerable<Mercury> GetAll()
         {
-            return _mercuriesList;
+            //return _mercuriesList;
+            return _context.Mercury.ToList();
         }
 
         public Mercury Find(string id)
         {
             int matchingId = int.Parse(id);
-            return _mercuriesList.SingleOrDefault(m => m.Mercury_Id == matchingId);
+            //return _mercuriesList.SingleOrDefault(m => m.Mercury_Id == matchingId);
+            return _context.Mercury.SingleOrDefault(m => m.Mercury_Id == matchingId);
         }
 
         public void Remove(string id)
         {
             int matchingId = int.Parse(id);
-            var itemToRemove = _mercuriesList.FirstOrDefault(m => m.Mercury_Id == matchingId);
+            //var itemToRemove = _mercuriesList.FirstOrDefault(m => m.Mercury_Id == matchingId);
+            //if (itemToRemove != null)
+            //    _mercuriesList.Remove(itemToRemove);
+            var itemToRemove = _context.Mercury.SingleOrDefault(m => m.Mercury_Id == matchingId);
             if (itemToRemove != null)
-                _mercuriesList.Remove(itemToRemove);
+            {
+                _context.Mercury.Remove(itemToRemove);
+            }
+            _context.SaveChanges();
         }
 
         public void Update(Mercury mercuryEntry)
         {
-            var itemToUpdate = _mercuriesList.FirstOrDefault(m => m.Mercury_Id == mercuryEntry.Mercury_Id);
+            //var itemToUpdate = _mercuriesList.FirstOrDefault(m => m.Mercury_Id == mercuryEntry.Mercury_Id);
+            //if (itemToUpdate != null)
+            //{
+            //    itemToUpdate.Mercury_Id = mercuryEntry.Mercury_Id;
+            //    itemToUpdate.DateTimeStart = mercuryEntry.DateTimeStart;
+            //    itemToUpdate.Hg = mercuryEntry.Hg;
+            //    itemToUpdate.Unit = mercuryEntry.Unit;
+            //}
+
+            var itemToUpdate = _context.Mercury.FirstOrDefault(m => m.Mercury_Id == mercuryEntry.Mercury_Id);
             if (itemToUpdate != null)
             {
                 itemToUpdate.Mercury_Id = mercuryEntry.Mercury_Id;
@@ -44,6 +72,7 @@ namespace AirQualityAPI.Repository
                 itemToUpdate.Hg = mercuryEntry.Hg;
                 itemToUpdate.Unit = mercuryEntry.Unit;
             }
+            _context.SaveChanges();
         }
     }
 }
--- Repository/OzonesRepository.cs
+++ Repository/OzonesRepository.cs
@@ -2,40 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AirQualityAPI.Contexts;
 using AirQualityAPI.Models;
 
 namespace AirQualityAPI.Repository
 {
     public class OzonesRepository : IOzonesRepository
     {
-        private static List<Ozone> _ozonesList = new List<Ozone>();
+        //private static List<Ozone> _ozonesList = new List<Ozone>();
+
+        private OzonesContext _context;
+
+        public OzonesRepository(OzonesContext context)
+        {
+            _context = context;
+        }
+
         public void Add(Ozone ozoneEntry)
         {
-            _ozonesList.Add(ozoneEntry);
+            //_ozonesList.Add(ozoneEntry);
+            _context.Ozone.Add(ozoneEntry);
+            _context.SaveChanges();
         }
 
         public IEnumerable<Ozone> GetAll()
         {
-            return _ozonesList;
+            //return _ozonesList;
+            return _context.Ozone.ToList();
         }
 
         public Ozone Find(string id)
         {
             int matchingId = int.Parse(id);
-            return _ozonesList.SingleOrDefault(o => o.Ozone_Id == matchingId);
+            //return _ozonesList.SingleOrDefault(o => o.Ozone_Id == matchingId);
+            return _context.Ozone.SingleOrDefault(o => o.Ozone_Id == matchingId);
         }
 
         public void Remove(string id)
         {
             int matchingId = int.Parse(id);
-            var itemToRemove = _ozonesList.FirstOrDefault(o => o.Ozone_Id == matchingId);
+            //var itemToRemove = _ozonesList.FirstOrDefault(o => o.Ozone_Id == matchingId);
+            //if (itemToRemove != null)
+            //    _ozonesList.Remove(itemToRemove);
+            var itemToRemove = _context.Ozone.FirstOrDefault(o => o.Ozone_Id == matchingId);
             if (itemToRemove != null)
-                _ozonesList.Remove(itemToRemove);
+                _context.Ozone.Remove(itemToRemove);
+            _context.SaveChanges();
         }
 
         public void Update(Ozone ozoneEntry)
         {
-            var itemToUpdate = _ozonesList.FirstOrDefault(o => o.Ozone_Id == ozoneEntry.Ozone_Id);
+            //var itemToUpdate = _ozonesList.FirstOrDefault(o => o.Ozone_Id == ozoneEntry.Ozone_Id);
+            //if (itemToUpdate != null)
+            //{
+            //    itemToUpdate.Ozone_Id = ozoneEntry.Ozone_Id;
+            //    itemToUpdate.DateTimeStart = ozoneEntry.DateTimeStart;
+            //    itemToUpdate.Ozone1 = ozoneEntry.Ozone1;
+            //    itemToUpdate.Unit = ozoneEntry.Unit;
+            //}
+
+            var itemToUpdate = _context.Ozone.FirstOrDefault(o => o.Ozone_Id == ozoneEntry.Ozone_Id);
             if (itemToUpdate != null)
             {
                 itemToUpdate.Ozone_Id = ozoneEntry.Ozone_Id;
@@ -43,6 +69,7 @@ namespace AirQualityAPI.Repository
                 itemToUpdate.Ozone1 = ozoneEntry.Ozone1;
                 itemToUpdate.Unit = ozoneEntry.Unit;
             }
+            _context.SaveChanges();
         }
     }
 }
