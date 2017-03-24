f0c303d4c160b2c200afdc03ad1f94ab4f3d9af2
Istvan Marki <istv0050@edu.easj.dk>
2017-03-24 20:03:25 +0100

    added model classes

---
+++ Models/Mercury.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirQualityAPI.Models
+{
+    public class Mercury
+    {
+        [Key]
+        public int Mercury_Id { get; set; }
+
+        public DateTime DateTimeStart { get; set; }
+
+        public double? Hg { get; set; }
+
+        public string Unit { get; set; }
+    }
+}
---
+++ Models/Ozone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirQualityAPI.Models
+{
+    public class Ozone
+    {
+        [Key]
+        public int Ozone_Id { get; set; }
+        public DateTime DateTimeStart { get; set; }
+        public double? Ozone1 { get; set; }
+        public string Unit { get; set; }
+    }
+}
