ed806ace8aeab4323ba8a1c4d9ff8ab4dbab0ee3
Istvan Marki <istv0050@edu.easj.dk>
2017-03-24 20:19:11 +0100

    adding controllers

---
+++ Controllers/MercuriesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AirQualityAPI.Models;
+using AirQualityAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirQualityAPI.Controllers
+{
+
+    [Produces("application/json")]
+    [Route("api/Mercuries")]
+    public class MercuriesController : Controller
+    {
+        public IMercuriesRepository MercuriesRepo { get; set; }
+
+        public MercuriesController(IMercuriesRepository _repo)
+        {
+            MercuriesRepo = _repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Mercury> GetAll()
+        {
+            return MercuriesRepo.GetAll().Take(500);
+        }
+
+        [HttpGet("{id}", Name = "GetMercuries")]
+        public IActionResult GetById(string id)
+        {
+            var item = MercuriesRepo.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Create([FromBody] Mercury item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            MercuriesRepo.Add(item);
+            return CreatedAtRoute("GetMercuries", new { Controller = "Mercuries", id = item.Mercury_Id }, item);
+        }
+
+        [HttpPut]
+        public IActionResult Update(string id, [FromBody] Mercury item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            var mercuryObj = MercuriesRepo.Find(id);
+            if (mercuryObj == null)
+            {
+                return NotFound();
+            }
+            MercuriesRepo.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            MercuriesRepo.Remove(id);
+        }
+    }
+}
---
+++ Controllers/OzonesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AirQualityAPI.Models;
+using AirQualityAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace AirQualityAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Ozones")]
+    public class OzonesController : Controller
+    {
+        public IOzonesRepository OzonesRepo { get; set; }
+
+        public OzonesController(IOzonesRepository _repo)
+        {
+            OzonesRepo = _repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Ozone> GetAll()
+        {
+            return OzonesRepo.GetAll().Take(500);
+        }
+
+        [HttpGet("{id}", Name = "GetOzones")]
+        public IActionResult GetById(string id)
+        {
+            var item = OzonesRepo.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Create([FromBody] Ozone item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            OzonesRepo.Add(item);
+            return CreatedAtRoute("GetOzones", new { Controller = "Ozones", id = item.Ozone_Id }, item);
+        }
+
+        [HttpPut]
+        public IActionResult Update(string id, [FromBody] Ozone item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            var ozoneObj = OzonesRepo.Find(id);
+            if (ozoneObj == null)
+            {
+                return NotFound();
+            }
+            OzonesRepo.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            OzonesRepo.Remove(id);
+        }
+    }
+}
