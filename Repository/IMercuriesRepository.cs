f352684c391f972b3bd412334cf1b443073906b0
Istvan Marki <istv0050@edu.easj.dk>
2017-03-24 20:07:38 +0100

    added interfaces

---
+++ Repository/IMercuriesRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AirQualityAPI.Models;
+
+namespace AirQualityAPI.Repository
+{
+    public interface IMercuriesRepository
+    {
+        void Add(Mercury mercuryEntry);
+        IEnumerable<Mercury> GetAll();
+        Mercury Find(string id);
+        void Remove(string id);
+        void Update(Mercury mercuryEntry);
+    }
+}
